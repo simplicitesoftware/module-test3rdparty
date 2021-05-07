71b1f8251a3aa4f8bab8c8b3bdb0a3770b6d5ff9
designer (Designer) <no-reply@simplicite.fr>
2021-05-07 22:09:26 +0200

    Indent

--- resources/ObjectExternal/T3pTestChart/SCRIPT.js
+++ resources/ObjectExternal/T3pTestChart/SCRIPT.js
@@ -1,22 +1,21 @@
 var T3pTestChart = (function() {
 	function chart(name, color) {
 		cytoscape({
-		  container: $("#T3pTestChart"),
-		  elements: [
-		  	{ data: { id: "a", label: name + "-a" } },
-		  	{ data: { id: "b", label: name + "-b" } },
-		  	{ data: { id: "c", label: name + "-c" } },
-		  	{ data: { id: "ab", source: "a", target: "b" } },
-		  	{ data: { id: "bc", source: "b", target: "c" } },
-		  	{ data: { id: "ca", source: "a", target: "c" } }
-		  ],
-		  style: [
-		    { selector: "node", style: { "background-color": color || "#ccc", "label": "data(label)" } },
-		    { selector: "edge", style: { "width": 3, "line-color": "#ccc", "target-arrow-color": "#ccc", "target-arrow-shape": "triangle" } }
-		  ],
-		  layout: { name: "grid", rows: 1 }
+			container: $("#T3pTestChart"),
+			elements: [
+				{ data: { id: "a", label: name + "-a" } },
+				{ data: { id: "b", label: name + "-b" } },
+				{ data: { id: "c", label: name + "-c" } },
+				{ data: { id: "ab", source: "a", target: "b" } },
+				{ data: { id: "bc", source: "b", target: "c" } },
+				{ data: { id: "ca", source: "a", target: "c" } }
+			],
+			style: [
+				{ selector: "node", style: { "background-color": color || "#ccc", "label": "data(label)" } },
+				{ selector: "edge", style: { "width": 3, "line-color": "#ccc", "target-arrow-color": "#ccc", "target-arrow-shape": "triangle" } }
+			],
+			layout: { name: "grid", rows: 1 }
 		});
-
 	}
 
 	return {
